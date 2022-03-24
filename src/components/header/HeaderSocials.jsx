2a7bd78c7b67d50ce2ddcefc6fae0c9e66f9f68c
VladislavPetruk <vladiislav.petruk@gmail.com>
2022-03-24 20:49:55 +0200

    change link

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,9 +5,9 @@ import {CgMail} from 'react-icons/cg'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="https://www.youtube.com/" target='_blank'><BsTelegram /></a>
-        <a href="https://www.youtube.com/" target='_blank'><BsGithub /></a>
-        <a href="https://www.youtube.com/" target='_blank'><CgMail /></a>
+        <a href="https://www.youtube.com/" target='blank'><BsTelegram /></a>
+        <a href="https://www.youtube.com/" target='blank'><BsGithub /></a>
+        <a href="https://www.youtube.com/" target='blank'><CgMail /></a>
     </div>
   )
 }
